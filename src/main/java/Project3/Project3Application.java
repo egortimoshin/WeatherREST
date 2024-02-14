dfde77ee122d1abdb5a10da491cf6fb3d908c382
Egor Timoshin <timoshinegor@inbox.ru>
2024-02-14 12:40:00 +0300

    add docker

--- src/main/java/Project3/Project3Application.java
+++ src/main/java/Project3/Project3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Project3Application.class, args);
 	}
